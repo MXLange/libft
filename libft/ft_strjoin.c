4d1f64ee9b82958add189bb3b61b93e9ed550892
Murillo Sprengel Lange <lange_sp@hotmail.com>
2022-11-05 14:33:09 +0000

    ft_strjoin

---
+++ libft/ft_strjoin.c
@@ -0,0 +1,36 @@
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	s1_count;
+	size_t	s2_count;
+	size_t	final_len;
+	char	*final;
+
+	final_len = ft_strlen(s1) + ft_strlen(s2) + 2;
+	final = malloc(sizeof(char) * (final_len));
+	if (!final)
+		return (NULL);
+	s1_count = 0;
+	s2_count = 0;
+	while (s1[s1_count])
+	{
+		final[s1_count] = s1[s1_count];
+		s1_count++;
+	}
+	while (s2[s2_count])
+		final[s1_count++] = s2[s2_count++];
+	final[s1_count] = '\0';
+	return (final);
+}
+
+int	main()
+{
+	char *a = "Vinho tinto ";
+	char *b = "do Porto.";
+	char *concat = ft_strjoin(a, b);
+	printf("A: %s + B: %s =\n", a, b);
+	printf("%s\n", concat);
+	return (0);
+}
