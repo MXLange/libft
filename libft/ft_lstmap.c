3476b374d7298610d6930dd6b3aa081a985e7c9f
Murillo Sprengel Lange <lange_sp@hotmail.com>
2022-11-08 18:28:44 +0000

    final H08NOV

---
+++ libft/ft_lstmap.c
@@ -0,0 +1,26 @@
+
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void*))
+{
+	t_list	*new_node;
+	t_list	*new_list;
+
+	if (!lst || !f || !del)
+		return (NULL);
+	if (!(new_list = ft_lstnew(f(lst->content))))
+		return (NULL);
+	lst = lst->next;
+	while (lst)
+	{
+		new_node = ft_lstnew(f(lst->content));
+		if (!new_node)
+		{
+			ft_lstclear(&new_list, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&new_list, new_node);
+		lst = lst->next;
+	}
+	return (new_list);
+}
--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: mu <mu@student.42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/02 15:18:29 by msprenge          #+#    #+#             */
-/*   Updated: 2022/11/08 16:53:44 by mu               ###   ########.fr       */
+/*   Updated: 2022/11/08 18:03:52 by mu               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,7 +67,8 @@ t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *new);
 void	ft_lstdelone(t_list *lst, void (*del)(void *));
 void	ft_lstclear(t_list **lst, void (*del)(void *));
-
+void	ft_lstiter(t_list *lst, void (*f)(void *));
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void*));
 
 
 
